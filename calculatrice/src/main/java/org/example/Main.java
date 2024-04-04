e5b3fee58d2618184f50a5794fdb7b688aaeeb11
loic <loic.gstrn@gmail.com>
2024-04-04 17:25:59 +0200

    calculatrice debut projet

---
+++ calculatrice/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        JFrame jFrame = new JFrame("Calculatrice 3000");
+        jFrame.setSize(750, 800);
+        jFrame.setLocationRelativeTo(null);
+
+        jFrame.setVisible(true);
+
+    }
+}
